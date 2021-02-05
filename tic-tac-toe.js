3cde88eee8e3556c02165de9c3aa1bd4bf801931
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-02-04 19:09:46 -0500

    fixed moves and tie. about to determine winner.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,25 +1,35 @@
 let x = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
 let o = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
 let currentPlayer = x;
+let turnNumber = 1;
+let gameStatus = null;
+window.addEventListener("DOMContentLoaded", event =>{
 
-
-let board = document.getElementById('tic-tac-toe-board');
-
-board.addEventListener('click', event => {
-
-    let clicked = event.target.id;
-
-    let img = document.createElement('img');
-    img.src = currentPlayer;
-    let target = document.getElementById(clicked);
-    target.appendChild(img);
-    //get the value of each square
-    if (currentPlayer === x) {
-        currentPlayer = o;
-
-    }
-    else if (currentPlayer === o) {
-        currentPlayer = x;
-    }
-
+    
+    let board = document.getElementById('tic-tac-toe-board');
+    
+    board.addEventListener('click', event => {
+        
+        let clicked = event.target.id;
+        
+        let img = document.createElement('img');
+        img.src = currentPlayer;
+        let target = document.getElementById(clicked);
+        target.appendChild(img);
+        turnNumber++;
+        //get the value of each square
+        if (turnNumber > 9) {
+            gameStatus = 'Tie';
+            h1 = document.getElementById('game-status');
+            h1.innerText = 'Tie game!'
+        }
+        if (currentPlayer === x) {
+            currentPlayer = o;
+            
+        }
+        else if (currentPlayer === o) {
+            currentPlayer = x;
+        }
+        
+    })
 });
