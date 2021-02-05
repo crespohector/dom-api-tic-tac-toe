3926c085c58946b55a3d3a1297457851f0ad2188
hector <crespomhector@gmail.com>
2021-02-04 19:18:53 -0500

    second commit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,13 +5,13 @@ let turnNumber = 1;
 let gameStatus = null;
 window.addEventListener("DOMContentLoaded", event =>{
 
-    
+
     let board = document.getElementById('tic-tac-toe-board');
-    
+
     board.addEventListener('click', event => {
-        
+
         let clicked = event.target.id;
-        
+
         let img = document.createElement('img');
         img.src = currentPlayer;
         let target = document.getElementById(clicked);
@@ -23,13 +23,24 @@ window.addEventListener("DOMContentLoaded", event =>{
             h1 = document.getElementById('game-status');
             h1.innerText = 'Tie game!'
         }
+        //create an array of winnning combinations
+        //assign clicks div's an extra class of current player
+        //iterate the array and compare current moves to winning array;
+        //if true; game status === 'win!'
+        //define winning game status to h1 tag;
         if (currentPlayer === x) {
             currentPlayer = o;
-            
+
         }
         else if (currentPlayer === o) {
             currentPlayer = x;
         }
-        
+
     })
+    //build the 'new game' and 'give up' button functionality
+    //add click event listeners to both buttons;
+    //define actions;
+    
+
+
 });
